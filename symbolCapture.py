3d50a3633365cdd4c8b6ea26684043964a1ec3a3
MScott1024 <scott.michael.s385@gmail.com>
2026-06-17 20:58:47 -0600

    Delete symbolCapture.py

--- symbolCapture.py
+++
@@ -1,9 +0,0 @@
-import cv2 as c
-from PIL import ImageGrab
-from datetime import datetime
-from time import sleep
-import numpy as n
-region = (1060,493,1180,675)
-sleep(2)
-symbol = c.cvtColor(n.array(ImageGrab.grab((region))), c.COLOR_BGR2GRAY)
-n.save(f"Symbols/{datetime.now().strftime('%H-%M-%S.%f')}",symbol)
